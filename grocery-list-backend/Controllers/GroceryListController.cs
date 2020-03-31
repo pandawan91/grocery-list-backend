25c83e1cf8e01a7aac40118d85c1c5c40284aad5
Vuong Ngo <info@vuongngo.com>
2020-03-31 11:20:50 +0200

    init solution, add swaggerui, add grocerylistcontroller, add ignorefile

---
+++ grocery-list-backend/Controllers/GroceryListController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace grocery_list_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroceryListController : ControllerBase
+    {
+        // GET: api/GroceryList
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST: api/GroceryList
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
